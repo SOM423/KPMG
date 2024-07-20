15e0c79bcaa2d97da89b4be03c85d36251fc8597
SOM423 <75077641+SOM423@users.noreply.github.com>
2024-07-21 00:53:23 +0530

    Create GroupAnagramStrings.java
    Group anagram Strings together

---
+++ GroupAnagramStrings.java
@@ -0,0 +1,40 @@
+public class Anagarm {
+
+	public static void main(String[] args) {
+		
+		String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> groups = groupAnagram(strs);
+        System.out.println(groups);
+		
+			
+
+	}
+	
+	//IMP Program to Practice
+	public static List<List<String>> groupAnagram(String[] str)
+	{
+		Map<String, List<String>> map = new HashMap<>();
+		
+		for(String s : str)
+		{
+			char[] charray = s.toCharArray();
+			 
+			
+			String sortedString = new String(charray);
+			
+			if(!map.containsKey(sortedString))
+			{
+				map.put(sortedString, new ArrayList<>());
+			}
+			
+			map.get(sortedString).add(s);
+			
+			
+		}
+		
+		List<List<String>> result = new ArrayList<>(map.values());
+		
+		return result;
+	}
+
+}
