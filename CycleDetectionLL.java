17938b66881dae86ad3b2f2a6347e69fba2518e7
SOM423 <75077641+SOM423@users.noreply.github.com>
2024-07-21 14:40:22 +0530

    CycleDetectionLL.java
    Check for cycle in Linked List

---
+++ CycleDetectionLL.java
@@ -0,0 +1,25 @@
+public class CycleDetectionLL {
+
+    public static boolean detectCycle(Node head) {
+        
+        if(head == null || head.next == null)
+        {
+            return false;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
