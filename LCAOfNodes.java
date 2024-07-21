0c18525635a564f4ad995199dad42aedb3aa9856
SOM423 <75077641+SOM423@users.noreply.github.com>
2024-07-21 14:20:42 +0530

    LCAOfNodes.java
    Find the LCA of Given Nodes

---
+++ LCAOfNodes.java
@@ -0,0 +1,33 @@
+public class LCAOfNodes {
+    public static BinaryTreeNode<Integer> lcaOfThreeNodes(BinaryTreeNode<Integer> root, int node1, int node2,
+            int node3) {
+        // base case
+
+        if((root==null) || (root.data == node1 || root.data == node2 || root.data == node3))
+        {
+            return root;
+        }
+
+        BinaryTreeNode<Integer> leftAns = lcaOfThreeNodes(root.left, node1, node2, node3);
+
+        BinaryTreeNode<Integer> rightAns = lcaOfThreeNodes(root.right, node1, node2, node3);
+        
+
+        if(leftAns != null  && rightAns != null)
+        {
+            return root;
+        }
+
+        else if(leftAns == null)
+        {
+            return rightAns;
+        }
+        else
+        {
+            return leftAns;
+        }
+
+
+    }
+
+}
